c57e81aaab589cd85306329e09464859e0d33a22
said-yousuf <saidyousuf.said@gmail.com>
2025-02-21 22:39:46 +0430

    feat: some minor changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,26 @@
-import { Stack } from "expo-router";
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack } from 'expo-router';
+import { useEffect } from 'react';
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    InterRegular: require('./assets/fonts/Inter-Regular.ttf'),
+    interBold: require('./assets/fonts/Inter-Bold.ttf'),
+    InterMedium: require('./assets/fonts/Inter-Medium.ttf'),
+    InterSemiBold: require('./assets/fonts/Inter-SemiBold.ttf'),
+    InterExtraBold: require('./assets/fonts/Inter-ExtraBold.ttf'),
+    InterLight: require('./assets/fonts/Inter-Light.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from 'react-native';
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text>Hello</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+});
