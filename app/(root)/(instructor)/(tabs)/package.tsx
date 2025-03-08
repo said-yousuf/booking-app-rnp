e01ae6dec3f26311aef6620559986d806aed252d
said-yousuf <saidyousuf.said@gmail.com>
2025-03-08 09:04:19 +0430

    chore: some minor changes

--- app/(root)/(instructor)/(tabs)/package.tsx
+++ app/(root)/(instructor)/(tabs)/package.tsx
@@ -1,12 +1,98 @@
+import FullPackage from '@/components/Full-Package';
+import HourlyPackage from '@/components/Hourly-Package';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
-const Package = () => {
+const Tab = createMaterialTopTabNavigator();
+
+type CustomLabelProps = {
+  focused: boolean;
+  children: string;
+};
+
+const CustomLabel: React.FC<CustomLabelProps> = ({ focused, children }) => (
+  <View
+    style={[
+      styles.labelContainer,
+      focused ? styles.activeTab : styles.inactiveTab,
+    ]}
+  >
+    <Text
+      style={[styles.label, focused ? styles.activeText : styles.inactiveText]}
+    >
+      {children}
+    </Text>
+  </View>
+);
+
+const PackageTabs = () => {
   return (
-    <View>
-      <Text>Package</Text>
-    </View>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: styles.tabBar,
+        tabBarItemStyle: styles.tab,
+        tabBarIndicatorStyle: { backgroundColor: 'transparent' },
+        tabBarLabel: ({ focused, children }) => (
+          <CustomLabel focused={focused}>{children as string}</CustomLabel>
+        ),
+      }}
+    >
+      <Tab.Screen
+        name="HourlyPackage"
+        component={HourlyPackage}
+        options={{ title: 'Hourly Package' }}
+      />
+      <Tab.Screen
+        name="FullPackage"
+        component={FullPackage}
+        options={{ title: 'Full Package' }}
+      />
+    </Tab.Navigator>
   );
 };
 
-export default Package;
+export default PackageTabs;
+
+const styles = StyleSheet.create({
+  tabBar: {
+    elevation: 0,
+    shadowOpacity: 0,
+    backgroundColor: '#CFCFDC',
+    width: 362,
+    height: 43,
+    borderRadius: 32,
+    alignSelf: 'center',
+    overflow: 'hidden',
+  },
+  tab: {
+    width: 178,
+    height: 39,
+  },
+  labelContainer: {
+    paddingHorizontal: 10,
+    paddingVertical: 6,
+    borderRadius: 32,
+  },
+  activeTab: {
+    backgroundColor: '#FFFFFF',
+    borderWidth: 1,
+    borderColor: '#CFCFDC',
+  },
+  inactiveTab: {
+    backgroundColor: '#CFCFDC',
+    color: '#fff',
+  },
+  label: {
+    fontSize: 14,
+    fontWeight: '600',
+    textAlign: 'center',
+  },
+  activeText: {
+    color: '#000',
+    fontWeight: 'bold',
+  },
+  inactiveText: {
+    color: '#fff',
+  },
+});
---
+++ components/Full-Package.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const FullPackage = () => {
+  return (
+    <View>
+      <Text>FullPackage</Text>
+    </View>
+  );
+};
+
+export default FullPackage;
